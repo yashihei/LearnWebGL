f5a628f5504007e99abf7bcf2e262d59550e4dee
yashihei <yashihei@gmail.com>
2019-08-11 02:17:19 +0900

    立体モデル(トーラス)の描画

--- script.js
+++ script.js
@@ -18,24 +18,10 @@ onload = function() {
     attStride[0] = 3;
     attStride[1] = 4;
 
-    var position = [
-        0.0, 1.0, 0.0,
-        1.0, 0.0, 0.0,
-        -1.0, 0.0, 0.0,
-        0.0, -1.0, 0.0
-    ];
-
-    var color = [
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0,
-        1.0, 1.0, 1.0, 1.0
-    ];
-
-    var index = [
-        0, 1, 2,
-        1, 2, 3
-    ];
+    var torusData = torus(32, 32, 1.0, 2.0);
+    var position = torusData[0];
+    var color = torusData[1];
+    var index = torusData[2];
 
     var pos_vbo = create_vbo(position);
     var col_vbo = create_vbo(color);
@@ -56,11 +42,15 @@ onload = function() {
     var tmpMatrix = m.identity(m.create());
     var mvpMatrix = m.identity(m.create());
 
-    m.lookAt([0.0, 0.0, 5.0], [0, 0, 0], [0, 1, 0], vMatrix);
+    m.lookAt([0.0, 0.0, 20.0], [0, 0, 0], [0, 1, 0], vMatrix);
     m.perspective(45, c.width / c.height, 0.1, 100, pMatrix);
     m.multiply(pMatrix, vMatrix, tmpMatrix);
 
     var count = 0;
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.CULL_FACE);
 
     (function() {
         gl.clearColor(0.0, 0.0, 1.0, 1.0);
@@ -72,7 +62,7 @@ onload = function() {
         var rad = (count % 360) * Math.PI / 180;
 
         m.identity(mMatrix);
-        m.rotate(mMatrix, rad, [0, 1, 0], mMatrix);
+        m.rotate(mMatrix, rad, [0, 1, 1], mMatrix);
         m.multiply(tmpMatrix, mMatrix, mvpMatrix);
         gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
 
@@ -148,5 +138,51 @@ onload = function() {
             gl.enableVertexAttribArray(attL[i]);
             gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
         }
+    }
+
+    function torus(row, column, irad, orad) {
+        var pos = new Array(), col = new Array(), idx = new Array();
+        for (var i = 0; i <= row; i++) {
+            var r = Math.PI * 2 / row * i;
+            var rr = Math.cos(r);
+            var ry = Math.sin(r);
+            for (var ii = 0; ii <= column; ii++) {
+                var tr = Math.PI * 2 / column * ii;
+                var tx = (rr * irad + orad) * Math.cos(tr);
+                var ty = ry * irad;
+                var tz = (rr * irad + orad) * Math.sin(tr);
+                pos.push(tx, ty, tz);
+                var tc = hsva(360 / column * ii, 1, 1, 1);
+                col.push(tc[0], tc[1], tc[2], tc[3]);
+            }
+        }
+        for (i = 0; i < row; i++) {
+            for (ii = 0; ii < column; ii++) {
+                r = (column + 1) * i + ii;
+                idx.push(r, r + column + 1, r + 1);
+                idx.push(r + column + 1, r + column + 2, r + 1);
+            }
+        }
+        return [pos, col, idx];
+    }
+
+    function hsva(h, s, v, a) {
+        if (s > 1 || v > 1 || a > 1) { return; }
+        var th = h % 360;
+        var i = Math.floor(th / 60);
+        var f = th / 60 - i;
+        var m = v * (1 - s);
+        var n = v * (1 - s * f);
+        var k = v * (1 - s * (1 - f));
+        var color = new Array();
+        if (!s > 0 && !s < 0) {
+            color.push(v, v, v, a);
+        } else {
+            var r = new Array(v, n, m, m, k, v);
+            var g = new Array(k, v, v, n, m, m);
+            var b = new Array(m, m, k, v, v, n);
+            color.push(r[i], g[i], b[i], a);
+        }
+        return color;
     }
 }
