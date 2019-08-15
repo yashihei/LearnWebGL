4e7090806767b5d7597894d96bda74788759f81c
yashihei <yashihei@gmail.com>
2019-08-15 19:05:09 +0900

    pugに置き換えた

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	mode: "development",
 	entry: "./src/script.ts",
@@ -13,10 +15,24 @@ module.exports = {
 			{
 				test: /\.ts$/,
 				use: "ts-loader"
+			},
+			{
+				test: /\.pug$/,
+				use: [
+					{
+						loader: 'pug-loader',
+						options: { pretty: true }
+					}
+				]
 			}
 		]
 	},
 	resolve: {
 		extensions: [".ts"]
-	}
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.pug"
+		})
+	]
 };
